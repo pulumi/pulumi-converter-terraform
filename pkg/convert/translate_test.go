00f18859fe34f9ee1e462e41348351c183c68290
Zaid Ajaj <zaid.naom@gmail.com>
2024-10-06 21:23:13 +0200

    remove test diagnostic that warns about missing local schema

--- pkg/convert/translate_test.go
+++ pkg/convert/translate_test.go
@@ -45,7 +45,8 @@ import (
 )
 
 type testLoader struct {
-	path string
+	path                   string
+	allowedMissingPackages []string
 }
 
 func (l *testLoader) LoadPackage(pkg string, version *semver.Version) (*schema.Package, error) {
@@ -57,6 +58,11 @@ func (l *testLoader) LoadPackage(pkg string, version *semver.Version) (*schema.P
 
 	schemaBytes, err := os.ReadFile(schemaPath)
 	if err != nil {
+		for _, allowedMissing := range l.allowedMissingPackages {
+			if allowedMissing == pkg {
+				return &schema.Package{Name: pkg, Version: version}, nil
+			}
+		}
 		return nil, err
 	}
 
@@ -115,7 +121,10 @@ func TestTranslate(t *testing.T) {
 		}
 	}
 
-	loader := &testLoader{path: filepath.Join(testDir, "schemas")}
+	loader := &testLoader{
+		path:                   filepath.Join(testDir, "schemas"),
+		allowedMissingPackages: []string{"kubernetes"},
+	}
 	mapper := &bridgetesting.TestFileMapper{Path: filepath.Join(testDir, "mappings")}
 
 	for _, tt := range tests {
