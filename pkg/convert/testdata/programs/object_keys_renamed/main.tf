39431bae0043d7a5e5c8de76abd54b330e62dea5
Brandon Pollack <brandonpollack23@gmail.com>
2024-11-02 01:27:05 +0900

    change to snake key to test local with snaked key name

--- pkg/convert/testdata/programs/object_keys_renamed/main.tf
+++ pkg/convert/testdata/programs/object_keys_renamed/main.tf
@@ -1,14 +1,14 @@
 locals {
-  obj = {
+  obj_key_snake = {
     "snake_case_of_local_no_mangle" = "abc123",
     snake_case_of_local_no_mangle_unquoted = "abc123",
   }
 }
 
 output "unmangled_locals" {
-  value = local.obj.snake_case_of_local_no_mangle
+  value = local.obj_key_snake.snake_case_of_local_no_mangle
 }
 
 output "unmangled_locals_unquote" {
-  value = local.obj.snake_case_of_local_no_mangle_unquoted
+  value = local.obj_key_snake.snake_case_of_local_no_mangle_unquoted
 }
--- pkg/convert/testdata/programs/object_keys_renamed/pcl/main.pp
+++ pkg/convert/testdata/programs/object_keys_renamed/pcl/main.pp
@@ -1,12 +1,12 @@
-obj = {
+obj_key_snake = {
   "snake_case_of_local_no_mangle"        = "abc123"
   snake_case_of_local_no_mangle_unquoted = "abc123"
 }
 
 output "unmangledLocals" {
-  value = obj.snake_case_of_local_no_mangle
+  value = obj_key_snake.snake_case_of_local_no_mangle
 }
 
 output "unmangledLocalsUnquote" {
-  value = obj.snake_case_of_local_no_mangle_unquoted
+  value = obj_key_snake.snake_case_of_local_no_mangle_unquoted
 }
