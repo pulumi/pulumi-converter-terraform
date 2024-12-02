bac6ed9f29ce949e28d0ce05fcf962ecd53d2c83
Brandon Pollack <brandonpollack23@gmail.com>
2024-12-02 18:55:49 +0900

    Add parameterization to pulumi package blocks.
    This adds parameterization for terraform providers which require dynamic
    bridging (and do not have pulumiverse or official pulumi supported
    packages already).

---
+++ pkg/convert/testdata/programs/required_providers_tf/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    tf-boundary = {
+      source = "hashicorp/boundary"
+      version = "1.1.9"
+    }
+  }
+  required_version = ">= 1.3.5"
+}
+
+output "asdfasdfasdf" {
+  value = 123
+}
