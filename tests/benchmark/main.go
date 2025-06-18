5e9d90f84fd1126118bba6711dc83de90597ae07
Venelin <venelin@pulumi.com>
2025-06-18 18:59:51 +0300

    Add languages and better result display for convert benchmark

--- tests/benchmark/main.go
+++ tests/benchmark/main.go
@@ -50,11 +50,62 @@ func formatResults(results map[string]*benchmarkResult) string {
 	return buf.String()
 }
 
+func resultSummary(results map[string]*benchmarkResult) string {
+	buf := bytes.Buffer{}
+	total := len(results)
+	type summary struct {
+		convertSuccesses int
+		planSuccesses    int
+		applySuccesses   int
+		assertSuccesses  int
+	}
+	res := summary{}
+	for _, v := range results {
+		if v.convertSuccess {
+			res.convertSuccesses++
+		}
+		if v.planSuccess {
+			res.planSuccesses++
+		}
+		if v.applySuccess {
+			res.applySuccesses++
+		}
+		if v.assertSuccess {
+			res.assertSuccesses++
+		}
+	}
+	buf.WriteString(fmt.Sprintf("total: %d\n", total))
+	buf.WriteString(fmt.Sprintf("convertSuccesses: %d (%d%%)\n", res.convertSuccesses, res.convertSuccesses*100/total))
+	buf.WriteString(fmt.Sprintf("planSuccesses: %d (%d%%)\n", res.planSuccesses, res.planSuccesses*100/total))
+	buf.WriteString(fmt.Sprintf("applySuccesses: %d (%d%%)\n", res.applySuccesses, res.applySuccesses*100/total))
+	buf.WriteString(fmt.Sprintf("assertSuccesses: %d (%d%%)\n", res.assertSuccesses, res.assertSuccesses*100/total))
+	return buf.String()
+}
+
 func main() {
 	tfResults := runTofuBenchmarks(testCases)
-	fmt.Printf("tfResults: %s", formatResults(tfResults))
-	pulumiResults := runPulumiBenchmarks(testCases, runPulumiConvert)
-	fmt.Printf("pulumiResults: %s", formatResults(pulumiResults))
+	fmt.Printf("tfResults:\n%s", formatResults(tfResults))
 	claudeResults := runPulumiBenchmarks(testCases, runClaudeConvert)
-	fmt.Printf("claudeResults: %s", formatResults(claudeResults))
+	fmt.Printf("claudeResults:\n%s", formatResults(claudeResults))
+	pulumiResultsTs := runPulumiBenchmarks(testCases, runPulumiConvertTS)
+	fmt.Printf("pulumiResultsTs:\n%s", formatResults(pulumiResultsTs))
+	pulumiResultsPy := runPulumiBenchmarks(testCases, runPulumiConvertPy)
+	fmt.Printf("pulumiResultsPy:\n%s", formatResults(pulumiResultsPy))
+	pulumiResultsGo := runPulumiBenchmarks(testCases, runPulumiConvertGo)
+	fmt.Printf("pulumiResultsGo:\n%s", formatResults(pulumiResultsGo))
+	pulumiResultsCs := runPulumiBenchmarks(testCases, runPulumiConvertCs)
+	fmt.Printf("pulumiResultsCs:\n%s", formatResults(pulumiResultsCs))
+	pulumiResultsJava := runPulumiBenchmarks(testCases, runPulumiConvertJava)
+	fmt.Printf("pulumiResultsJava:\n%s", formatResults(pulumiResultsJava))
+	pulumiResultsYaml := runPulumiBenchmarks(testCases, runPulumiConvertYaml)
+	fmt.Printf("pulumiResultsYaml:\n%s", formatResults(pulumiResultsYaml))
+	fmt.Println("--------------------------------")
+	fmt.Printf("tfResults:\n%s", resultSummary(tfResults))
+	fmt.Printf("claudeResults:\n%s", resultSummary(claudeResults))
+	fmt.Printf("pulumiResultsTs:\n%s", resultSummary(pulumiResultsTs))
+	fmt.Printf("pulumiResultsPy:\n%s", resultSummary(pulumiResultsPy))
+	fmt.Printf("pulumiResultsGo:\n%s", resultSummary(pulumiResultsGo))
+	fmt.Printf("pulumiResultsCs:\n%s", resultSummary(pulumiResultsCs))
+	fmt.Printf("pulumiResultsJava:\n%s", resultSummary(pulumiResultsJava))
+	fmt.Printf("pulumiResultsYaml:\n%s", resultSummary(pulumiResultsYaml))
 }
