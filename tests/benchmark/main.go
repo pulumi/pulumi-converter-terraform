8c1df3508dc9dad8354a0716416876e826a8c6d0
VenelinMartinov <venelin.v.martinov@gmail.com>
2025-06-24 13:24:03 +0300

    Add AWS lambda example to conversion benchmark (#340)
    This adds a non-trivial AWS lambda example to the conversion benchmark.
    Adapted from
    https://developer.hashicorp.com/terraform/tutorials/aws/lambda-api-gateway

--- tests/benchmark/main.go
+++ tests/benchmark/main.go
@@ -4,45 +4,11 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"time"
 )
-
-var testCases = []testCase{
-	{
-		name: "random_simple",
-		dir:  "programs/random_simple",
-		assertion: func(output map[string]any) error {
-			if output["name"] == nil {
-				return fmt.Errorf("name is nil")
-			}
-			if len(output["name"].(string)) == 0 {
-				return fmt.Errorf("name is empty")
-			}
-			return nil
-		},
-	},
-	{
-		name: "aws_bucket",
-		dir:  "programs/aws_bucket",
-		assertion: func(output map[string]any) error {
-			if output["url"] == nil {
-				return fmt.Errorf("url is nil")
-			}
-			if len(output["url"].(string)) == 0 {
-				return fmt.Errorf("url is empty")
-			}
-			out, err := run(".", "aws", "s3", "cp", output["url"].(string), "-")
-			if err != nil {
-				return fmt.Errorf("failed to copy object: %w", err)
-			}
-			if string(out) != "hi" {
-				return fmt.Errorf("expected 'hi', got %s", string(out))
-			}
-
-			return nil
-		},
-	},
-}
 
 func formatResults(results map[string]*benchmarkResult) string {
 	buf := bytes.Buffer{}
@@ -84,17 +50,22 @@ func resultSummary(results map[string]*benchmarkResult) string {
 	return buf.String()
 }
 
-func runBenchmarkForLanguage(language string) {
+func runBenchmarkForLanguage(language string, skipTF bool, testCases []testCase) {
 	switch language {
 	case "typescript":
-		tfResults := runTofuBenchmarks(testCases)
-		fmt.Printf("tfResults:\n%s", formatResults(tfResults))
+		tfResults := map[string]*benchmarkResult{}
+		if !skipTF {
+			tfResults = runTofuBenchmarks(testCases)
+			fmt.Printf("tfResults:\n%s", formatResults(tfResults))
+		}
 		claudeResults := runPulumiBenchmarks(testCases, runClaudeConvert)
 		fmt.Printf("claudeResults:\n%s", formatResults(claudeResults))
 		pulumiResultsTs := runPulumiBenchmarks(testCases, runPulumiConvertTS)
 		fmt.Printf("pulumiResultsTs:\n%s", formatResults(pulumiResultsTs))
 		fmt.Println("--------------------------------")
-		fmt.Printf("tfResults:\n%s", resultSummary(tfResults))
+		if !skipTF {
+			fmt.Printf("tfResults:\n%s", resultSummary(tfResults))
+		}
 		fmt.Printf("claudeResults:\n%s", resultSummary(claudeResults))
 		fmt.Printf("pulumiResultsTs:\n%s", resultSummary(pulumiResultsTs))
 	default:
@@ -104,9 +75,13 @@ func runBenchmarkForLanguage(language string) {
 	}
 }
 
-func runBenchmark() {
-	tfResults := runTofuBenchmarks(testCases)
-	fmt.Printf("tfResults:\n%s", formatResults(tfResults))
+func runBenchmark(skipTF bool, testCases []testCase) {
+	tfResults := map[string]*benchmarkResult{}
+	if !skipTF {
+		tfResults = runTofuBenchmarks(testCases)
+		fmt.Printf("tfResults:\n%s", formatResults(tfResults))
+	}
+
 	claudeResults := runPulumiBenchmarks(testCases, runClaudeConvert)
 	fmt.Printf("claudeResults:\n%s", formatResults(claudeResults))
 	pulumiResultsTs := runPulumiBenchmarks(testCases, runPulumiConvertTS)
@@ -122,7 +97,9 @@ func runBenchmark() {
 	pulumiResultsYaml := runPulumiBenchmarks(testCases, runPulumiConvertYaml)
 	fmt.Printf("pulumiResultsYaml:\n%s", formatResults(pulumiResultsYaml))
 	fmt.Println("--------------------------------")
-	fmt.Printf("tfResults:\n%s", resultSummary(tfResults))
+	if !skipTF {
+		fmt.Printf("tfResults:\n%s", resultSummary(tfResults))
+	}
 	fmt.Printf("claudeResults:\n%s", resultSummary(claudeResults))
 	fmt.Printf("pulumiResultsTs:\n%s", resultSummary(pulumiResultsTs))
 	fmt.Printf("pulumiResultsPy:\n%s", resultSummary(pulumiResultsPy))
@@ -132,12 +109,105 @@ func runBenchmark() {
 	fmt.Printf("pulumiResultsYaml:\n%s", resultSummary(pulumiResultsYaml))
 }
 
+var allTestCases = map[string]testCase{
+	"random_simple": {
+		name: "random_simple",
+		dir:  "programs/random_simple",
+		assertion: func(output map[string]any) error {
+			if output["name"] == nil {
+				return fmt.Errorf("name is nil")
+			}
+			if len(output["name"].(string)) == 0 {
+				return fmt.Errorf("name is empty")
+			}
+			return nil
+		},
+	},
+	"aws_bucket": {
+		name: "aws_bucket",
+		dir:  "programs/aws_bucket",
+		assertion: func(output map[string]any) error {
+			if output["url"] == nil {
+				return fmt.Errorf("url is nil")
+			}
+			if len(output["url"].(string)) == 0 {
+				return fmt.Errorf("url is empty")
+			}
+			out, err := run(".", "aws", "s3", "cp", output["url"].(string), "-")
+			if err != nil {
+				return fmt.Errorf("failed to copy object: %w", err)
+			}
+			if string(out) != "hi" {
+				return fmt.Errorf("expected 'hi', got %s", string(out))
+			}
+
+			return nil
+		},
+	},
+	"aws_lambda_api": {
+		name: "aws_lambda_api",
+		dir:  "programs/aws_lambda_api",
+		assertion: func(output map[string]any) error {
+			time.Sleep(2 * time.Second)
+
+			if output["url"] == nil {
+				return fmt.Errorf("url is nil")
+			}
+
+			url := output["url"].(string)
+			if len(url) == 0 {
+				return fmt.Errorf("url is empty")
+			}
+
+			// make an http request to the url
+			resp, err := http.Get(url + "/hello?Name=John")
+			if err != nil {
+				return fmt.Errorf("failed to make http request: %w", err)
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != 200 {
+				return fmt.Errorf("expected status code 200, got %d", resp.StatusCode)
+			}
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				return fmt.Errorf("failed to read response body: %w", err)
+			}
+
+			expected := `{"message":"Hello, John!"}`
+			if string(body) != expected {
+				return fmt.Errorf("expected %s, got %s", expected, string(body))
+			}
+
+			return nil
+		},
+	},
+}
+
 func main() {
 	language := flag.String("language", "typescript", "The language to benchmark. all will run all languages")
+	skipTF := flag.Bool("skip-tf", false, "Skip the Terraform benchmark")
+	example := flag.String("example", "all", "The example to run. all will run all examples")
 	flag.Parse()
-	if *language == "all" {
-		runBenchmark()
+
+	testCases := []testCase{}
+	if *example == "all" {
+		for _, v := range allTestCases {
+			testCases = append(testCases, v)
+		}
 	} else {
-		runBenchmarkForLanguage(*language)
+		tCase, ok := allTestCases[*example]
+		if !ok {
+			fmt.Printf("Example %s not found\n", *example)
+			os.Exit(1)
+		}
+		testCases = []testCase{tCase}
+	}
+
+	if *language == "all" {
+		runBenchmark(*skipTF, testCases)
+	} else {
+		runBenchmarkForLanguage(*language, *skipTF, testCases)
 	}
 }
---
+++ tests/benchmark/programs/aws_lambda_api/main.tf
@@ -0,0 +1,84 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    archive = {
+      source = "hashicorp/archive"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_pet" "lambda_bucket_name" {
+  prefix = "learn-terraform-functions"
+  length = 4
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = random_pet.lambda_bucket_name.id
+}
+
+resource "aws_s3_bucket_ownership_controls" "lambda_bucket" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "lambda_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.lambda_bucket]
+
+  bucket = aws_s3_bucket.lambda_bucket.id
+  acl    = "private"
+}
+
+data "archive_file" "lambda_hello_world" {
+  type = "zip"
+
+  source_dir  = "${path.root}/hello-world"
+  output_path = "${path.root}/hello-world.zip"
+}
+
+resource "aws_s3_object" "lambda_hello_world" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "hello-world.zip"
+  source = data.archive_file.lambda_hello_world.output_path
+
+  etag = filemd5(data.archive_file.lambda_hello_world.output_path)
+}
+
+resource "random_string" "lambda_name" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+resource "aws_lambda_function" "hello_world" {
+  function_name = "HelloWorld-${random_string.lambda_name.result}"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_hello_world.key
+
+  runtime = "nodejs20.x"
+  handler = "hello.handler"
+
+  source_code_hash = data.archive_file.lambda_hello_world.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+
+resource "aws_cloudwatch_log_group" "hello_world" {
+  name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
+
+  retention_in_days = 3
+}
