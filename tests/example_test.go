91d31f278ebddad4cbac87e347eca96e0c010b29
Anton Tayanovskyy <anton@pulumi.com>
2025-06-25 12:01:01 -0400

    Fully remove

--- tests/example_test.go
+++ tests/example_test.go
@@ -302,8 +302,7 @@ func TestExample(t *testing.T) {
 		{
 			example: "https://github.com/terraform-aws-modules/terraform-aws-s3-bucket",
 			strict:  true,
-			//testOnlyThisExampleInLanguage: "typescript",
-			commit: "f90d8a385e4c70afd048e8997dcccf125b362236",
+			commit:  "f90d8a385e4c70afd048e8997dcccf125b362236",
 		},
 		{
 			example: "https://github.com/terraform-aws-modules/terraform-aws-s3-bucket/examples/object",
