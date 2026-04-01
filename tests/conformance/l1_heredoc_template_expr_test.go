9bd42dca29e87b1249703de8b5fcf6bbc53f885b
Ian Wahbe <me@iwahbe.com>
2026-04-01 16:39:47 +0200

    Convert `heredoc_template_expr` to a conformance test

---
+++ tests/conformance/l1_heredoc_template_expr_test.go
@@ -0,0 +1,45 @@
+// Copyright 2026, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package conformance
+
+import (
+	"testing"
+
+	"github.com/pulumi/pulumi-converter-terraform/pkg/testing/conformance"
+)
+
+func TestL1HeredocTemplateExpr(t *testing.T) {
+	t.Parallel()
+	conformance.AssertConversion(t, conformance.TestCase{
+		HCL: `
+locals {
+  a_list = ["a", "b", "c"]
+  join_template_expr = <<EOT
+%{for v in local.a_list~}
+${v}
+%{endfor~}
+EOT
+
+  tuple_cons_heredoc = [
+<<EOT
+oh baby give me
+one more chance
+to show you that I love you
+EOT
+  ]
+}
+`,
+	})
+}
