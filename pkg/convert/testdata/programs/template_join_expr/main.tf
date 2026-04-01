de3e814e023bd3f857d52f8f3a08155e14532254
Ian Wahbe <me@iwahbe.com>
2026-04-01 16:40:03 +0200

    Convert `template_join_expr` to a conformance test

--- pkg/convert/testdata/programs/template_join_expr/main.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  a_list = ["a", "b", "c"]
-  join_template_expr = "%{for v in local.a_list~}${v}%{endfor~}"
-  if_template_expr = "%{if true~}true%{else~}false%{endif~}"
-}
---
+++ tests/conformance/l1_template_join_expr_test.go
@@ -0,0 +1,34 @@
+// Copyright 2026, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package conformance
+
+import (
+	"testing"
+
+	"github.com/pulumi/pulumi-converter-terraform/pkg/testing/conformance"
+)
+
+func TestL1TemplateJoinExpr(t *testing.T) {
+	t.Parallel()
+	conformance.AssertConversion(t, conformance.TestCase{
+		HCL: `
+locals {
+  a_list = ["a", "b", "c"]
+  join_template_expr = "%{for v in local.a_list~}${v}%{endfor~}"
+  if_template_expr = "%{if true~}true%{else~}false%{endif~}"
+}
+`,
+	})
+}
